125eeb88bdb09e9ad510217ddc2ee1db113699e9
Rick Verbon <rick89@gmail.com>
2023-03-30 22:21:16 +0200

    First commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <router-view :user="user"></router-view>
+  </div>
+</template>
+
+<script>
+import router from './router/router'
+
+export default {
+  name: 'app',
+  router,
+  data() {
+    return {
+      user: null
+    }
+  }
+}
+</script>
